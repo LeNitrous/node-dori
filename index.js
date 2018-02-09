914ccb2532e3cf43c2375fb05d2722c7e64a33a4
LeNitrous <n20gaming2000@gmail.com>
2018-02-09 15:26:13 +0800

    handle promise rejection

--- index.js
+++ index.js
@@ -38,105 +38,129 @@ class BandoriApi {
 
     getCards() {
         return new Promise((resolve, reject) => {
-            this.query('/card').then(response => {
-                var cardArray = [];
-                response.data.forEach(elem => {
-                    cardArray.push(new Card(elem, this.region))
+            this.query('/card')
+                .then(response => {
+                    var cardArray = [];
+                    response.data.forEach(elem => {
+                        cardArray.push(new Card(elem, this.region))
+                    });
+                    resolve(cardArray);
+                })
+                .catch(error => {
+                    throw error;
                 });
-                resolve(cardArray);
-            }).catch(reject);
-        }).catch(reject);
+        });
     }
 
     getCardByID(id) {
         return new Promise((resolve, reject) => {
-            this.query('/card').then(response => {
-                var search = {
-                    cardId: id
-                };
+            this.query('/card')
+                .then(response => {
+                    var search = {
+                        cardId: id
+                    };
 
-                if (Object.keys(search).length == 0)
-                    reject(new InvalidParameterError());
-                
-                var match = response.data.filter(o => {
-                    return Object.keys(search).every(k => {
-                        return o[k] === search[k];
-                    });
-                }).shift();
+                    if (Object.keys(search).length == 0)
+                        reject(new InvalidParameterError());
+                    
+                    var match = response.data.filter(o => {
+                        return Object.keys(search).every(k => {
+                            return o[k] === search[k];
+                        });
+                    }).shift();
 
-                if (match == undefined)
-                    reject(new EmptyResponseError());
-
-                this.query(`/card/${match.cardId}`).then(response => {
-                    resolve(new Card(response, this.region));
-                }).catch(reject);
-            }).catch(reject);
-        })
+                    if (match == undefined)
+                        reject(new EmptyResponseError());
+                    else
+                    this.query(`/card/${match.cardId}`)
+                        .then(response => {
+                            resolve(new Card(response, this.region));
+                        })
+                        .catch(error => {
+                            throw error;
+                        });
+                })
+                .catch(error => {
+                    throw error;
+                });
+        });
     }
 
     getCardByQuery(query) {
         return new Promise((resolve, reject) => {
-            this.query('/card').then(response => {
-                var nameFormat = query.find(str => { return str.match(/[a-zA-z]+[1-4]/g) });
-                var allowAttrs = ['powerful', 'power', 'pure', 'happy', 'cool'];
-                var allowMemberNames = [];
-                Object.values(this.constants.Characters)
-                    .forEach((elem, index) => {
-                        if (index == 0) return;
-                        allowMemberNames[index] = elem.split(' ').pop().toLowerCase();
+            this.query('/card')
+                .then(response => {
+                    var nameFormat = query.find(str => { return str.match(/[a-zA-z]+[1-4]/g) });
+                    var allowAttrs = ['powerful', 'power', 'pure', 'happy', 'cool'];
+                    var allowMemberNames = [];
+                    Object.values(this.constants.Characters)
+                        .forEach((elem, index) => {
+                            if (index == 0) return;
+                            allowMemberNames[index] = elem.split(' ').pop().toLowerCase();
+                        });
+        
+                    var rarity, id, attr;
+
+                    id = query.filter(str => { return allowMemberNames.includes(str.toLowerCase()) }).shift();
+                    rarity = (query.find(num => { return num.match(/[1-4]/g) })) ? 
+                        query.find(num => { return num.match(/[1-4]/g) }) : undefined;
+                    attr = query.filter(str => allowAttrs.includes(str.toLowerCase())).shift();
+                    attr = (attr == 'power') ? 'powerful' : attr;
+
+                    var search = {};
+                    if (id)
+                        search.characterId = allowMemberNames.indexOf(id) + 1;
+                    if (rarity)
+                        search.rarity = parseInt(rarity);
+                    if (attr)
+                        search.attr = attr;
+
+                    if (Object.keys(search).length == 0)
+                        reject(new InvalidParameterError());
+
+                    var match = response.data.filter(o => {
+                        return Object.keys(search).every(k => {
+                            return o[k] === search[k];
+                        });
                     });
-    
-                var rarity, id, attr;
 
-                id = query.filter(str => { return allowMemberNames.includes(str.toLowerCase()) }).shift();
-                rarity = (query.find(num => { return num.match(/[1-4]/g) })) ? 
-                    query.find(num => { return num.match(/[1-4]/g) }) : undefined;
-                attr = query.filter(str => allowAttrs.includes(str.toLowerCase())).shift();
-                attr = (attr == 'power') ? 'powerful' : attr;
-
-                var search = {};
-                if (id)
-                    search.characterId = allowMemberNames.indexOf(id) + 1;
-                if (rarity)
-                    search.rarity = parseInt(rarity);
-                if (attr)
-                    search.attr = attr;
-
-                if (Object.keys(search).length == 0)
-                    reject(new InvalidParameterError());
-
-                var match = response.data.filter(o => {
-                    return Object.keys(search).every(k => {
-                        return o[k] === search[k];
+                    match.sort((a, b) => {
+                        if (a.rarity > b.rarity) return -1;
+                        if (a.rarity < b.rarity) return 1;
+                        return 0;
                     });
-                });
 
-                match.sort((a, b) => {
-                    if (a.rarity > b.rarity) return -1;
-                    if (a.rarity < b.rarity) return 1;
-                    return 0;
+                    this.query(`/card/${match[0].cardId}`)
+                        .then(response => {
+                            var result = [];
+                            result.push(new Card(response, this.region));
+                            match.shift();
+                            if (match.length > 0)
+                                match.forEach(card => { result.push(new Card(card, this.region)) });
+                            resolve(result);
+                        })
+                        .catch(error => {
+                            throw error;
+                        });
+                })
+                .catch(error => {
+                    throw error;
                 });
-
-                this.query(`/card/${match[0].cardId}`).then(response => {
-                    var result = [];
-                    result.push(new Card(response, this.region));
-                    match.shift();
-                    if (match.length > 0)
-                        match.forEach(card => { result.push(new Card(card, this.region)) });
-                    resolve(result);
-                }).catch(reject)
-            }).catch(reject);
         });
     }
 
     getCurrentEvent() {
         return new Promise((resolve, reject) => {
-            this.query('/event').then(response => {
-                if (!response)
-                    reject(new EmptyResponseError())
-                resolve(new Event(response, this.region));
-            }).catch(reject);
-        })
+            this.query('/event')
+                .then(response => {
+                    if (!response)
+                        reject(new EmptyResponseError())
+                    resolve(new Event(response, this.region));
+                })
+                .catch(error => {
+                    throw error;
+                });
+        });
     }
 }
 
